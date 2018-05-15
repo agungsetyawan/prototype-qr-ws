aa4cc72aea4a8324e48b52763e03036a92bc855f
Agung Setyawan <agung.setyawan@mirumagency.com>
2018-05-15 11:46:06 +0700

    Revert "Revert "update 0.1""
    This reverts commit e2c2397db0102aaa9451ded501b3cdc34df1e972.

--- app.js
+++ app.js
@@ -7,20 +7,19 @@ var mongoose = require('mongoose');
 var socket = require('socket.io');
 var uniqid = require('uniqid');
 var moment = require('moment');
-moment.locale('id');
 var qr = require('qr-image');
+moment.locale('id');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var chatsRouter = require('./routes/chats');
 var testsRouter = require('./routes/tests');
 
 var testModel = require('./models/test_model');
 
 var app = express();
-var server = app.listen(3000, function() {
-  console.log('listening to requests on port 3000');
-});
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,7 +34,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/chats', chatsRouter);
 app.use('/test', testsRouter);
 
@@ -55,9 +53,17 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+module.exports = {
+  app: app,
+  server: server
+};
 
 // =============================================================================
+
+// init
+var hostname = process.env.HOSTNAME;
+var timeFormat = process.env.TIME_FORMAT;
 
 // database MongoDB
 var connectionDB = 'mongodb://localhost:27017/mylearning';
@@ -71,42 +77,52 @@ mongoose.connect(connectionDB, function() {
 });
 
 // socket.io
-var io = socket(server);
 io.on('connection', function(socket) {
-  console.log('+socket:', socket.id, 'connected');
-
-  // uniqid
-  var uniqueID = uniqid();
-  var time = moment().format('YYYY-MM-DD, HH:mm:ss');
-  var link = 'http://localhost:3000/test?id=' + uniqueID;
-  var svg_string = qr.imageSync(link, {
-    type: 'svg'
-  });
-  var linkData = {
-    uniqid: uniqueID,
-    socket: socket.id,
-    created_at: time,
-    opened_at: '',
-    opened: false,
-    qr: svg_string
+  function create() {
+    console.log('+socket:', socket.id, 'connected');
+    // uniqid
+    var uniqueID = uniqid();
+    var time = moment().format(timeFormat);
+    var link = 'http://' + hostname + ':3000/test/' + uniqueID;
+    var svg_string = qr.imageSync(link, {
+      type: 'svg'
+    });
+    var linkData = {
+      uniqid: uniqueID,
+      socket: socket.id,
+      created_at: time,
+      opened_at: '',
+      opened: false,
+      qr: svg_string
+    }
+    // add to database
+    testModel.create(linkData);
   }
-  testModel.create(linkData);
+  create();
 
   socket.on('disconnect', function() {
     console.log('-socket:', socket.id, 'disconnected');
-    testModel.remove({
+    var query = {
       socket: socket.id,
       opened: false
-    }, function(err) {
+    };
+    //remove from database
+    testModel.remove(query, function(err) {
       if (err) return handleError(err);
       // removed!
     });
   });
+
   socket.on('chat', function(data) {
     // hanya kirim ke orang lain
     socket.broadcast.emit('chat', data);
 
     //kirim ke diri sendiri juga
     // io.sockets.emit('chat', data);
+  });
+
+  socket.on('qr', function(data) {
+    // create();
+    io.sockets.emit('qr', create);
   });
 });
