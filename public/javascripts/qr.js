9b1fbd3f6c92b551ed88a17551021e76b683b474
Agung Setyawan <agung.setyawan@mirumagency.com>
2018-05-24 11:22:23 +0700

    add geolocation

--- public/javascripts/qr.js
+++ public/javascripts/qr.js
@@ -25,4 +25,52 @@
     $("#printQR").removeClass('d-none');
     $("#printQR").html(`${qrObj.qr}`);
   }
+
+  if (navigator.geolocation) {
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //   var pos = {
+    //     lat: position.coords.latitude,
+    //     lng: position.coords.longitude
+    //   };
+    //   console.log(position);
+    //   console.log(pos);
+    // }, function() {
+    //   console.log('err')
+    //   // handleLocationError(true, infoWindow, map.getCenter());
+    // });
+    var id, target, options;
+
+    function success(pos) {
+      var crd = pos.coords;
+      console.log(crd);
+      console.log(crd.latitude);
+      console.log(crd.longitude);
+
+      if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+        console.log('Congratulations, you reached the target');
+        navigator.geolocation.clearWatch(id);
+      }
+    }
+
+    function error(err) {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    }
+
+    target = {
+      latitude: 0,
+      longitude: 0
+    };
+
+    options = {
+      enableHighAccuracy: false,
+      timeout: 500000,
+      maximumAge: 0
+    };
+
+    id = navigator.geolocation.watchPosition(success, error);
+  } else {
+    // Browser doesn't support Geolocation
+    console.log('not support')
+    // handleLocationError(false, infoWindow, map.getCenter());
+  }
 </script>
